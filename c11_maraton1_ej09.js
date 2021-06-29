54c6e66fc42a2eb952a73cd60e7c1e1e9ad7cb66
jtizziani <64490459+jtizziani@users.noreply.github.com>
2021-06-28 21:28:49 -0300

    Actualización

--- c11_maraton1_ej09.js
+++ c11_maraton1_ej09.js
@@ -14,4 +14,39 @@ si el nro de comensales son mas de 4 adultos se hace un 10% de descuento del tot
 
 
 */
+function descuento(total_fact){
+         desc =  total_fact *(10/100);
+         return desc
 
+}
+
+let adul = 0;
+let meno = 0;
+var desc;
+var total_fact = 0;
+const ej9restaurant = [25,17,18,44,12,9,36,50]
+alert(ej9restaurant);
+
+for (let i =0; i<ej9restaurant.length; i++){
+     //alert(ej9restaurant[i]);
+     if (ej9restaurant[i] > 17){
+          adul = adul + 1;
+          total_fact = total_fact + 700;
+
+     }
+     if ((ej9restaurant[i] < 18)){
+          meno = meno + 1;
+          total_fact = total_fact + 450;
+     } 
+}
+comensales = adul +meno;
+if (adul >= 4){
+    descuento(total_fact);
+    alert("Los comensales son: "+comensales+"\n Comensales monores "+meno+"\n Comensales Mayores "+adul+"\n Total Facturado $"+total_fact+"\n Descuento $"+desc+"\n Total a Pagar $"+(total_fact-desc));
+
+}
+if (adul < 4){
+    desc = 0;
+    alert("Los comensales son: "+comensales+"\n Comensales monores "+meno+"\n Comensales Mayores "+adul+"\n Total Facturado $"+total_fact+"\n Descuento $"+desc+"\n Total a Pagar $"+(total_fact-desc));
+
+}
